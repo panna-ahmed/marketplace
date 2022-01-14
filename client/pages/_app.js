fd7ac9d1d9dd882d559070e0d36e4ce5c04a0edc
Saleh Ahmed Panna <panna.ahmed09@gmail.com>
2022-01-14 07:13:39 +0600

    Initial project folder structure setup

---
+++ client/pages/_app.js
@@ -0,0 +1,9 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'antd/dist/antd.css';
+import '../public/css/styles.css';
+
+function MarketPlaceApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
+
+export default MarketPlaceApp;
---
+++ client/pages/index.js
@@ -0,0 +1,9 @@
+const Index = () => {
+  return (
+    <>
+      <h1 className="jumbotron text-center bg-primary">Marketa</h1>
+    </>
+  );
+};
+
+export default Index;
